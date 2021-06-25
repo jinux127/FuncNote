6f91f4e27b7f60e2386f7daff5e1da680ff92333
jinwoo <wwo1278@gmail.com>
2021-06-25 15:08:31 +0900

    Locale List 업데이트

--- LocaleList/src/GenerateLocaleList/LocaleList.java
+++ LocaleList/src/GenerateLocaleList/LocaleList.java
@@ -5,27 +5,25 @@ public class LocaleList {
 
 	public static void main(String[] args) {
 		Locale locale;
-//		System.out.println(locale.getDisplayCountry());//현재 설정된 나라언어
-//		System.out.println(locale.getDisplayName());//현재 설정된 언어 및 나라
-//		System.out.println(locale.getLanguage());//현재 설정된 언어
-//		locale.getDisplayName(Locale.KOREA)// 현재 설정된 언어를 Locale.*의 언어의 형식으로 출력
-//		for(Locale locale : Locale.getAvailableLocales()) {
-//			System.out.println(locale.getISOCountries());
-//			System.out.println(locale.getISOLanguages());
-//		}
-//		for(String country : Locale.getISOCountries()) { //ISO 코드 출력
-//			System.out.println("========================================");
-//			System.out.println(country);//ISO 표준 나라 코드
-//			locale = new Locale("ko",country); //언어, 나라 설정 
-//			
-//			System.out.println(locale.getDisplayName());
-//		}
 		
-		for(String language : Locale.getISOLanguages()) { //ISO 코드 출력
-            System.out.println("========================================");
-            System.out.println(language);//ISO 표준 나라 코드
-            locale = new Locale(language,"KR"); //언어, 나라 설정 
-            System.out.println(locale.getDisplayName());
+		String language = "fr"; // 언어선택
+
+		for(String country : Locale.getISOCountries()) { //선택된 언어로 나라리스트 출력
+            locale = new Locale(language,country);
+            System.out.println(country); // ISO 코드
+            System.out.println(locale.getDisplayCountry(new Locale(language))); // 나라명
+            System.out.println("------------------");
+		}
+		
+		for (Locale locale2 : Locale.getAvailableLocales()) { // 선택된 언어를 사용하는 나라 목록
+			if(language == locale2.getLanguage()) {
+				System.out.println("getLanguage : " + locale2.getLanguage());
+                System.out.println("getCountry : " + locale2.getCountry()); 
+                System.out.println("getDisplayName : " + locale2.getDisplayName());
+                System.out.println("getDisplayLanguage : " + locale2.getDisplayLanguage());
+                System.out.println("getDisplayCountry : " + locale2.getDisplayCountry());
+                System.out.println("===================");
+	        }
 		}
 		
 		
